3c271510edbb8ff53b8b101f664652a5d0ac40a4
Caio Geraldes <caioaguida@gmail.com>
2020-07-14 12:02:17 -0300

    Plato annotated, 2 deleted examples.

--- dataexp.R
+++ dataexp.R
@@ -25,3 +25,18 @@ atuaplot2 <- function () {
     ggplot(data, aes(attraction, fill=pressuposition)) +
         geom_bar() + facet_wrap(~author) 
 }
+
+
+atuaplot3 <- function () {
+    print(table(data$pressuposition, data$attraction))
+    print(assocstats(table(data$pressuposition, data$attraction)))
+    ggplot(data, aes(attraction, fill=pressuposition)) +
+        geom_bar() + facet_wrap(~poss_verb) 
+}
+
+atuapres <- function (num, val) {
+    print(data$pressuposition[num])
+    data$pressuposition[num] <- val
+    print(data$pressuposition[num])
+    return(data)
+}
